e431f84f171cd92febd2249af928b4cc84e467af
RazakKhir <razak.khir@yahoo.com>
2022-09-17 14:09:19 +0800

    Initial commit

---
+++ src/test/java/com/calculator/shippingrate/ShippingRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.shippingrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
